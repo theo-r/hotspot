74ce7b39f6e96f2917f689d1094f54ec3e4944d9
theorutter <theo.rutter@gmail.com>
2021-12-27 21:55:50 +0000

    Add unit test for transform lambda

---
+++ ingest/test/test_ingest.py
@@ -0,0 +1,4 @@
+from ingest.ingest import IngestManager
+
+def test_ingest_manager():
+    assert 1 == 1
---
+++ transform/test/test_transform.py
@@ -0,0 +1,31 @@
+from mock import patch
+from transform.transform import lambda_handler
+import json
+import os
+from unittest.mock import MagicMock
+
+def read_spotify_response_helper(*args, **kwargs):
+    with open("transform/test/data/res.json", "r") as f:
+        res = json.load(f)
+    return res
+
+def _get_event():
+    with open("transform/test/data/event.json", "r") as f:
+        event = json.load(f)
+    return event
+
+
+@patch(
+    'transform.transform.TransformManager.read_spotify_response',
+    new=read_spotify_response_helper
+)
+@patch(
+    'transform.transform.TransformManager.push_data',
+    new=MagicMock(return_value="200")
+)
+def test_lambda_handler():
+    os.environ["GLUE_TABLE_NAME"] = ""
+    os.environ["GLUE_DB_NAME"] = ""
+    event = _get_event()
+    res = lambda_handler(event, "")
+    assert res == "200"
