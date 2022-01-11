b6ce7a769e5dfcbca81c322d6e458febec7899b9
theorutter <theo.rutter@gmail.com>
2022-01-11 18:15:53 +0000

    Add account env variables

--- aws/app.py
+++ aws/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import os
 import aws_cdk as cdk
 from hotspot_stack import HotspotStack
 
-
+account = os.environ.get("AWS_ACCOUNT")
+region = os.environ.get("AWS_REGION")
 app = cdk.App()
-HotspotStack(app, "HotspotStack")
+HotspotStack(app, "HotspotStack", env=cdk.Environment(account=account, region=region))
 app.synth()
