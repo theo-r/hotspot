309aec18c32de15a07858f3b8e2959f92a317280
theorutter <theo.rutter@gmail.com>
2022-01-12 10:04:05 +0000

    Try setting environment variable

--- aws/app.py
+++ aws/app.py
@@ -4,7 +4,7 @@ import aws_cdk as cdk
 from hotspot_stack import HotspotStack
 
 account = os.environ.get("AWS_ACCOUNT")
-region = os.environ.get("AWS_REGION")
+region = os.environ.get("AWS_DEFAULT_REGION")
 app = cdk.App()
 HotspotStack(app, "HotspotStack", env=cdk.Environment(account=account, region=region))
 app.synth()
