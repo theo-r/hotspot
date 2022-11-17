ff0260bb86b2609ab793393cfec1cefe5c457cec
Theo <Theo@macbook-pro.home>
2022-11-17 21:29:45 +0000

    Hide first data load

--- hotspot/hotspot.py
+++ hotspot/hotspot.py
@@ -1,20 +1,14 @@
-import logging
 import boto3
 import awswrangler as wr
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 import streamlit as st
-from typing import Tuple
 import os
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 boto3.setup_default_session(region_name="eu-west-1")
 
 cols = ["name", "artist_name", "album_name", "played_at", "user_name"]
-
 users = ["Dan", "Fred", "George", "Theo"]
 
 st.set_page_config(layout="wide")
@@ -24,8 +18,7 @@ glue_db = os.environ.get("GLUE_DB")
 glue_table = os.environ.get("GLUE_TABLE")
 workgroup = os.environ.get("WORKGROUP")
 
-
-@st.cache(ttl=3600)
+@st.cache(ttl=3600, show_spinner=False)
 def load_data():
     df = wr.athena.read_sql_query(
         f"SELECT * FROM {glue_table}",
@@ -37,38 +30,44 @@ def load_data():
     df = df.astype({"explicit": "object"})
     return df
 
+def main():
+    user_name = st.sidebar.selectbox(
+        label="Select a user", options=users + ["All"],
+        key="user_name"
+    )
 
-user_name = st.sidebar.selectbox(
-    label="Select a user", options=users + ["All"], index=3
-)
+    start_date = st.sidebar.date_input(
+        "Start date:",
+        value=datetime.now() + timedelta(days=-7),
+        min_value=datetime(2021, 1, 1),
+        max_value=datetime.now(),
+        key="start"
+    )
 
-df: pd.DataFrame = load_data()
-df.to_csv("data.csv")
+    end_date = st.sidebar.date_input(
+        "End date:",
+        value=datetime.now(),
+        min_value=start_date + timedelta(days=1),
+        max_value=datetime.now(),
+        key="end"
+    )
 
-start_date = st.sidebar.date_input(
-    "Start date:",
-    value=datetime.now() + timedelta(days=-7),
-    min_value=datetime(2021, 1, 1),
-    max_value=datetime.now(),
-)
+    start: datetime = datetime(start_date.year, start_date.month, start_date.day)
+    end: datetime = datetime(end_date.year, end_date.month, end_date.day) + timedelta(days=1)
 
-end_date = st.sidebar.date_input(
-    "End date:",
-    value=datetime.now(),
-    min_value=start_date + timedelta(days=1),
-    max_value=datetime.now(),
-)
+    num_days = (end - start).days
+    dates_index = pd.DataFrame(
+        {"dates": [(start + timedelta(days=x)).date() for x in range(num_days)]}
+    )
 
-start = datetime(start_date.year, start_date.month, start_date.day)
-end = datetime(end_date.year, end_date.month, end_date.day) + timedelta(days=1)
+    st.sidebar.button("Run", key="first_run")
 
-num_days = (end - start).days
-dates_index = pd.DataFrame(
-    {"dates": [(start + timedelta(days=x)).date() for x in range(num_days)]}
-)
+    df: pd.DataFrame = load_data()
 
+    if "first_run" in st.session_state and "user_name" in st.session_state and "start" in st.session_state and "end" in st.session_state:
+       render_page(df=df, user_name=user_name, start=start, end=end, dates_index=dates_index) 
 
-def get_top_artists(df: pd.DataFrame, user_name: str):
+def get_top_artists(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         return (
             df["artist_name"][df["played_at"] > start][df["played_at"] <= end]
@@ -88,13 +87,7 @@ def get_top_artists(df: pd.DataFrame, user_name: str):
             .rename(columns={"index": "artist", "artist_name": "plays"})
         )
 
-
-top_artists = get_top_artists(df=df, user_name=user_name)
-
-distinct_artists = len(set(top_artists["artist"].to_list()))
-
-
-def get_top_tracks(df: pd.DataFrame, user_name: str):
+def get_top_tracks(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         return (
             df["name"][df["played_at"] > start][df["played_at"] <= end]
@@ -114,11 +107,7 @@ def get_top_tracks(df: pd.DataFrame, user_name: str):
             .rename(columns={"index": "track", "name": "plays"})
         )
 
-
-top_tracks = get_top_tracks(df=df, user_name=user_name)
-
-
-def get_top_albums(df: pd.DataFrame, user_name: str):
+def get_top_albums(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         return (
             df["album_name"][df["played_at"] > start][df["played_at"] <= end]
@@ -138,11 +127,7 @@ def get_top_albums(df: pd.DataFrame, user_name: str):
             .rename(columns={"index": "album", "album_name": "plays"})
         )
 
-
-top_albums = get_top_albums(df=df, user_name=user_name)
-
-
-def get_top_genres(df: pd.DataFrame, user_name: str):
+def get_top_genres(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         genres_df = (
             df["genres"][df["played_at"] > start][df["played_at"] <= end]
@@ -168,11 +153,7 @@ def get_top_genres(df: pd.DataFrame, user_name: str):
         .sort_values(by="genre")
     )
 
-
-top_genres = get_top_genres(df=df, user_name=user_name)
-
-
-def get_top_album_images(df: pd.DataFrame, user_name: str):
+def get_top_album_images(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         return (
             df[["album_name", "album_image"]][df["played_at"] > start][
@@ -192,17 +173,7 @@ def get_top_album_images(df: pd.DataFrame, user_name: str):
             .reset_index(drop=False)
         )
 
-
-top_album_images = get_top_album_images(df=df, user_name=user_name)
-
-top10_albums = top_album_images[:10]["album_image"].to_list()
-distinct_albums = len(set(top_albums["album"].to_list()))
-
-if distinct_albums < 1:
-    st.write("Something went wrong: number of distinct albums less than 1")
-
-
-def get_all_tracks(df: pd.DataFrame, user_name: str):
+def get_all_tracks(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         return df[["name", "duration_ms"]][df["played_at"] > start][
             df["played_at"] <= end
@@ -212,14 +183,7 @@ def get_all_tracks(df: pd.DataFrame, user_name: str):
             df["played_at"] > start
         ][df["played_at"] <= end].reset_index(drop=False)
 
-
-all_tracks = get_all_tracks(df=df, user_name=user_name)
-
-num_tracks = all_tracks.shape[0]
-duration = all_tracks["duration_ms"].sum() / 3600000
-
-
-def get_listens_per_day(df: pd.DataFrame, user_name: str):
+def get_listens_per_day(df: pd.DataFrame, user_name: str, start: datetime, end: datetime, dates_index: pd.DataFrame):
     if user_name == "All":
         dfs = [
             df[df["user_name"] == user][df["played_at"] > start][df["played_at"] <= end]
@@ -248,11 +212,7 @@ def get_listens_per_day(df: pd.DataFrame, user_name: str):
         .replace(np.nan, 0)
     )
 
-
-listens_per_day = get_listens_per_day(df=df, user_name=user_name)
-
-
-def get_listens_by_hour_of_day(df: pd.DataFrame, user_name: str):
+def get_listens_by_hour_of_day(df: pd.DataFrame, user_name: str, start: datetime, end: datetime, num_tracks: int):
     if user_name == "All":
         return (
             df[df["played_at"] > start][df["played_at"] <= end]
@@ -272,11 +232,7 @@ def get_listens_by_hour_of_day(df: pd.DataFrame, user_name: str):
             * 100
         )
 
-
-listens_by_hour_of_day = get_listens_by_hour_of_day(df=df, user_name=user_name)
-
-
-def get_latest_tracks(df: pd.DataFrame, user_name: str):
+def get_latest_tracks(df: pd.DataFrame, user_name: str, start: datetime, end: datetime):
     if user_name == "All":
         return df.sort_values(by="played_at", ascending=False)[cols][
             df["played_at"] > start
@@ -286,47 +242,67 @@ def get_latest_tracks(df: pd.DataFrame, user_name: str):
             df["user_name"] == user_name
         ][df["played_at"] > start][df["played_at"] <= end].reset_index(drop=True)
 
+def render_page(df: pd.DataFrame, user_name: str, start: datetime, end: datetime, dates_index: pd.DataFrame):
+    top_artists = get_top_artists(df=df, user_name=user_name, start=start, end=end)
+    distinct_artists = len(set(top_artists["artist"].to_list()))
+    top_tracks = get_top_tracks(df=df, user_name=user_name, start=start, end=end)
+    top_albums = get_top_albums(df=df, user_name=user_name, start=start, end=end)
+    top_genres = get_top_genres(df=df, user_name=user_name, start=start, end=end)
+    top_album_images = get_top_album_images(df=df, user_name=user_name, start=start, end=end)
+    top10_albums = top_album_images[:10]["album_image"].to_list()
+    distinct_albums = len(set(top_albums["album"].to_list()))
 
-latest_tracks = get_latest_tracks(df=df, user_name=user_name)
+    if distinct_albums < 1:
+        st.write("Something went wrong: number of distinct albums less than 1")
 
-c1, c2, c3, c4, c5 = st.columns(5)
-c1.image(top10_albums[0])
-c2.image(top10_albums[1])
-c3.image(top10_albums[2])
-c4.image(top10_albums[3])
-c5.image(top10_albums[4])
+    all_tracks = get_all_tracks(df=df, user_name=user_name, start=start, end=end)
+    num_tracks = all_tracks.shape[0]
+    duration = all_tracks["duration_ms"].sum() / 3600000 
+    listens_per_day = get_listens_per_day(df=df, user_name=user_name, start=start, end=end, dates_index=dates_index)
+    listens_by_hour_of_day = get_listens_by_hour_of_day(df=df, user_name=user_name, start=start, end=end, num_tracks=num_tracks)
+    latest_tracks = get_latest_tracks(df=df, user_name=user_name, start=start, end=end)
 
-st.title("Hotspot")
+    c1, c2, c3, c4, c5 = st.columns(5)
+    c1.image(top10_albums[0])
+    c2.image(top10_albums[1])
+    c3.image(top10_albums[2])
+    c4.image(top10_albums[3])
+    c5.image(top10_albums[4])
 
-column1, column2 = st.columns(2)
-column1.header(f"Number of tracks: {num_tracks}")
-column2.header(f"Number of unique artists: {distinct_artists}")
-column1.header(f"Number of unique albums: {distinct_albums}")
-column2.header(f"Hours of play time: {round(duration, 1)}")
+    st.title("Hotspot")
+
+    column1, column2 = st.columns(2)
+    column1.header(f"Number of tracks: {num_tracks}")
+    column2.header(f"Number of unique artists: {distinct_artists}")
+    column1.header(f"Number of unique albums: {distinct_albums}")
+    column2.header(f"Hours of play time: {round(duration, 1)}")
+
+    col1, col2, col3 = st.columns(3)
+    col1.header("Top tracks")
+    col1.dataframe(top_tracks)
+    col2.header("Top artists")
+    col2.dataframe(top_artists)
+    col3.header("Top albums")
+    col3.dataframe(top_albums)
+
+    st.header("Listens per day")
+    st.line_chart(listens_per_day, use_container_width=True)
+
+    st.header("Listens by hour (%)")
+    st.bar_chart(listens_by_hour_of_day, use_container_width=True)
+
+    st.bar_chart(top_genres, use_container_width=True)
+
+    st.header("Latest tracks")
+    st.dataframe(latest_tracks)
+
+    c1, c2, c3, c4, c5 = st.columns(5)
+    c1.image(top10_albums[5])
+    c2.image(top10_albums[6])
+    c3.image(top10_albums[7])
+    c4.image(top10_albums[8])
+    c5.image(top10_albums[9])
 
 
-col1, col2, col3 = st.columns(3)
-col1.header("Top tracks")
-col1.dataframe(top_tracks)
-col2.header("Top artists")
-col2.dataframe(top_artists)
-col3.header("Top albums")
-col3.dataframe(top_albums)
-
-st.header("Listens per day")
-st.line_chart(listens_per_day, use_container_width=True)
-
-st.header("Listens by hour (%)")
-st.bar_chart(listens_by_hour_of_day, use_container_width=True)
-
-st.bar_chart(top_genres, use_container_width=True)
-
-st.header("Latest tracks")
-st.dataframe(latest_tracks)
-
-c1, c2, c3, c4, c5 = st.columns(5)
-c1.image(top10_albums[5])
-c2.image(top10_albums[6])
-c3.image(top10_albums[7])
-c4.image(top10_albums[8])
-c5.image(top10_albums[9])
+if __name__ == "__main__":
+    main()
