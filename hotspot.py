56e3c5b9d685b705dae7079e672ad490769c8986
Theo <theo.rutter@gmail.com>
2025-12-22 16:39:54 +0000

    Remove sidebar

--- hotspot.py
+++ hotspot.py
@@ -41,24 +41,27 @@ def load_data():
 
 
 def main():
-    user_name = st.sidebar.selectbox(
-        label="Select a user",
-        options=["All"] + users,
-    )
-
-    start_date = st.sidebar.date_input(
+    start_date = st.date_input(
         "Start date:",
         value=datetime.now() + timedelta(days=-365),
         min_value=datetime.now() + timedelta(days=-365),
         max_value=datetime.now(),
     )
 
-    end_date = st.sidebar.date_input(
+    end_date = st.date_input(
         "End date:",
         value=datetime.now(),
         min_value=start_date + timedelta(days=1),
         max_value=datetime.now(),
     )
+
+    user_names = st.pills(
+        label="Select users", options=users, selection_mode="multi", default=users
+    )
+
+    if len(user_names) < 1:
+        st.error("Please select at least 1 user")
+        return
 
     start: datetime = datetime(start_date.year, start_date.month, start_date.day)
     end: datetime = datetime(end_date.year, end_date.month, end_date.day) + timedelta(
@@ -80,7 +83,7 @@ def main():
     df: pd.DataFrame = load_data()
     render_page(
         df=df,
-        user_name=user_name,
+        user_names=user_names,
         start=start,
         end=end,
         dates_index=dates_index,
@@ -90,36 +93,34 @@ def main():
 
 def render_page(
     df: pd.DataFrame,
-    user_name: str,
+    user_names: list,
     start: datetime,
     end: datetime,
     dates_index: pd.DataFrame,
     month_years: list[str],
 ):
-    top_artists = get_top_artists(df=df, user_name=user_name, start=start, end=end)
+    top_artists = get_top_artists(df=df, user_names=user_names, start=start, end=end)
     distinct_artists = len(set(top_artists["artist"].to_list()))
-    top_tracks = get_top_tracks(df=df, user_name=user_name, start=start, end=end)
-    top_albums = get_top_albums(df=df, user_name=user_name, start=start, end=end)
-    top_genres = get_top_genres(df=df, user_name=user_name, start=start, end=end)
-    top_album_images = get_top_album_images(
-        df=df, user_name=user_name, start=start, end=end
-    )
-    top10_albums = top_album_images[:10]["album_image"].to_list()
+    top_tracks = get_top_tracks(df=df, user_names=user_names, start=start, end=end)
+    top_albums = get_top_albums(df=df, user_names=user_names, start=start, end=end)
+    top_genres = get_top_genres(df=df, user_names=user_names, start=start, end=end)
     distinct_albums = len(set(top_albums["album_name"].to_list()))
 
     if distinct_albums < 1:
         st.write("Something went wrong: number of distinct albums less than 1")
 
-    all_tracks = get_all_tracks(df=df, user_name=user_name, start=start, end=end)
+    all_tracks = get_all_tracks(df=df, user_names=user_names, start=start, end=end)
     num_tracks = all_tracks.shape[0]
     duration = all_tracks["duration_ms"].sum() / 3600000
     listens_per_day = get_listens_per_day(
-        df=df, user_name=user_name, start=start, end=end, dates_index=dates_index
+        df=df, user_names=user_names, start=start, end=end, dates_index=dates_index
     )
     # listens_by_hour_of_day = get_listens_by_hour_of_day(
     # df=df, user_name=user_name, start=start, end=end, num_tracks=num_tracks
     # )
-    latest_tracks = get_latest_tracks(df=df, user_name=user_name, start=start, end=end)
+    latest_tracks = get_latest_tracks(
+        df=df, user_names=user_names, start=start, end=end
+    )
 
     with st.container(horizontal=True, gap="large"):
         cols = st.columns(2, gap="medium", width=500)
@@ -158,34 +159,20 @@ def render_page(
 
     with cols[0].container(border=True, height="stretch"):
         st.text("Listens per day")
-        if user_name == "All":
-            st.altair_chart(
-                alt.Chart(listens_per_day)
-                .mark_line(size=1)
-                .transform_window(
-                    avg_listens="mean(listens)",
-                    frame=[-14, 0],
-                    groupby=["user"],
-                )
-                .encode(
-                    alt.X("date:T"),
-                    alt.Y("avg_listens:Q").title("avg listens in last 2 weeks"),
-                    alt.Color("user:N"),
-                )
+        st.altair_chart(
+            alt.Chart(listens_per_day)
+            .mark_line(size=1)
+            .transform_window(
+                avg_listens="mean(listens)",
+                frame=[-14, 0],
+                groupby=["user"],
             )
-        else:
-            st.altair_chart(
-                alt.Chart(listens_per_day)
-                .mark_line(size=1)
-                .transform_window(
-                    avg_listens="mean(listens)",
-                    frame=[-14, 0],
-                )
-                .encode(
-                    alt.X("date:T"),
-                    alt.Y("avg_listens:Q").title("avg listens in last 2 weeks"),
-                )
+            .encode(
+                alt.X("date:T"),
+                alt.Y("avg_listens:Q").title("avg listens in last 2 weeks"),
+                alt.Color("user:N"),
             )
+        )
 
     with cols[1].container(border=True, height="stretch"):
         st.text("Genres")
@@ -197,7 +184,7 @@ def render_page(
             )
         )
 
-    if user_name == "All":
+    if len(user_names) > 1:
         cols = st.columns(2)
         with cols[0].container(border=True, height="stretch"):
             st.text("Listen Distribution")
@@ -243,8 +230,7 @@ def render_page(
                 with inner_cols[1]:
                     st.markdown(f"**{t["artist_name"]}**")
                     st.markdown(f"{t["name"]}")
-                    if user_name == "All":
-                        st.badge(t["user_name"], color=user_colours[t["user_name"]])
+                    st.badge(t["user_name"], color=user_colours[t["user_name"]])
                 with inner_cols[2]:
                     st.metric(label="plays", value=t["count"])
 
@@ -258,8 +244,7 @@ def render_page(
                 with inner_cols[1]:
                     st.markdown(f"**{t["artist_name"]}**")
                     st.markdown(f"{t["album_name"]}")
-                    if user_name == "All":
-                        st.badge(t["user_name"], color=user_colours[t["user_name"]])
+                    st.badge(t["user_name"], color=user_colours[t["user_name"]])
                 with inner_cols[2]:
                     st.metric(label="plays", value=t["count"])
 
@@ -273,8 +258,7 @@ def render_page(
                 with inner_cols[1]:
                     st.markdown(f"**{t["artist_name"]}**")
                     st.markdown(f"{t["name"]}")
-                    if user_name == "All":
-                        st.badge(t["user_name"], color=user_colours[t["user_name"]])
+                    st.badge(t["user_name"], color=user_colours[t["user_name"]])
 
 
 if __name__ == "__main__":
